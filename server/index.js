e280c3c1f71248ca56da1d8367f7438b0a53dfe7
Arild Andreassen <arildsa@hotmail.com>
2021-01-01 16:18:24 -0700

    before api call

--- server/index.js
+++ server/index.js
@@ -1,25 +1,29 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const gameRouter = require('./routes/game-router');
+const cors = require("cors");
 
 const db = require('./db')
 
 const app = express();
 const apiPort = 3000;
 
-// app.set('view engine', 'ejs');
-
+app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json())
-// app.use(express.static("public"));
+app.use(express.static("public"));
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
-app.get('/', (req,res) => {
-    res.send('hello world');
-})
+// app.get('/', (req,res) => {
+//     res.send('hello world');
+// })
 
-app.use('/api', gameRouter)
+// app.get('/api/game', (req,res) => {
+//     res.send('new game created');
+// })
+
+// app.use('/api', gameRouter)
 
 app.listen(apiPort,() => {
     console.log('Server listening on port 3000')
